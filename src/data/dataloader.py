e9f3df4a9d6e9de2d700982eecf1bef1e515c987
at7095 <at7095@nyu.edu>
2026-04-17 18:56:48 -0400

    data: combined dataloader for NSynth + MoisesDB

---
+++ src/data/dataloader.py
@@ -0,0 +1,41 @@
+import torch
+from torch.utils.data import Dataset, DataLoader, ConcatDataset
+
+
+class NSynthWithIdentity(Dataset):
+    """Wraps NSynthBass and adds a dummy identity=-1"""
+    def __init__(self, nsynth_dataset):
+        self.ds = nsynth_dataset
+
+    def __len__(self):
+        return len(self.ds)
+
+    def __getitem__(self, idx):
+        sample = self.ds[idx]
+        sample["identity"] = torch.tensor(-1, dtype=torch.long)
+        return sample
+
+
+class MoisesDBWithPitch(Dataset):
+    """Wraps MoisesDB and adds a dummy pitch=-1"""
+    def __init__(self, moisesdb_dataset):
+        self.ds = moisesdb_dataset
+
+    def __len__(self):
+        return len(self.ds)
+
+    def __getitem__(self, idx):
+        sample = self.ds[idx]
+        sample["pitch"] = torch.tensor(-1, dtype=torch.long)
+        return sample
+
+
+def get_train_loader(nsynth_ds, moisesdb_ds, batch_size=32, num_workers=0):
+    nsynth_wrapped = NSynthWithIdentity(nsynth_ds)
+    moisesdb_wrapped = MoisesDBWithPitch(moisesdb_ds)
+    combined = ConcatDataset([nsynth_wrapped, moisesdb_wrapped])
+    return DataLoader(combined, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=True)
+
+
+def get_eval_loader(dataset, batch_size=64, num_workers=0):
+    return DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=True)
